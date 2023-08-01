cd0d00288033cd2eb01c19cc0574b2f39ec46a6e
mooner1022 <moonmk10221@gmail.com>
2023-08-01 19:56:30 +0900

    Add downloadable URL and modal

---
+++ src/routes/Modal.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let showModal = false;
+
+</script>
+
+{#if showModal}
+<div class="modal is-active" id="modal">
+    <div class="modal-background" on:click></div>
+    <div class="modal-card">
+        <div class="modal-card-body" id="body">
+            <slot name="content"/>
+            <button class="button is-dark" on:click>확인</button>
+        </div>
+    </div>
+</div>
+{/if}
+
+<style>
+    #modal, ::backdrop {
+        overscroll-behavior: contain;
+    }
+
+    #modal > .modal-card {
+        height: auto;
+    }
+
+    .modal-card > #body {
+        border-radius: 1.5em;
+        justify-content: center;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #body > button {
+        position: static;
+        bottom: 1em;
+        margin-left: auto;
+        margin-right: auto;
+        width: 90%;
+        border-radius: 1em;
+        height: 3.5em;
+    }
+</style>
--- src/routes/Title.svelte
+++ src/routes/Title.svelte
@@ -1,3 +1,26 @@
+<script>
+    import Modal from "./Modal.svelte";
+
+    const DOWNLOAD_URL = "https://cdn.discordapp.com/attachments/1106997962879545364/1135781787520466994/app-release.apk";
+
+    function toggleModal() {
+        showModal = !showModal;
+        document.body.style.position = showModal ? "fixed" : "static";
+        if (showModal)
+            downloadFile(DOWNLOAD_URL, "StarLight-v0_0_1.apk");
+    }
+
+    function downloadFile(url, name) {
+        const aTag = document.createElement("a");
+        aTag.href = url;
+        aTag.download = name;
+        document.body.appendChild(aTag);
+        aTag.click();
+        aTag.remove();
+    }
+
+    let showModal = false;
+</script>
 <section id="title">
     <div class="has-text-white">
         <h1 class="heading is-size-1 is-size-3-mobile">Project ✦</h1>
@@ -17,15 +40,29 @@
                 <div class="has-text-grey">Latest version</div>
                 <div class="is-size-4 has-text-weight-bold">v0.0.1-alpha</div>
             </div>
-            <a class="level-right icon-text has-text-weight-bold has-text-white p-4 pr-5 mr-1" id="button-download">
-                    <span class="icon">
-                        <i class="fa-solid fa-download"/>
-                    </span>
+            <div class="level-right icon-text has-text-weight-bold has-text-white p-4 pr-5 mr-1" id="button-download" tabindex="0" role="button" on:click={toggleModal} on:keypress={toggleModal}>
+                <span class="icon">
+                    <i class="fa-solid fa-download"/>
+                </span>
                 <span>Download</span>
-            </a>
+            </div>
         </div>
     </div>
     <div id="title-arrow" class="is-centered">
         <img src="assets/arrow.svg" alt="arrow">
     </div>
+    <Modal {showModal} on:click={toggleModal}>
+        <div slot="content" class="has-text-centered">
+            <div class="content mt-4" style="font-size: 1.5em;">
+                <span class="icon is-large" style="font-size: 2.5em; color: #ad977a">
+                    ✦
+                </span>
+                <br>
+                다운로드가 시작되었어요!
+            </div>
+            <div class="mb-5" style="font-size: 1.1em">
+                다운로드가 자동으로 시작되지 않았다면, <a href="{DOWNLOAD_URL}"><u>여기</u></a>를 클릭해주세요.
+            </div>
+        </div>
+    </Modal>
 </section>
